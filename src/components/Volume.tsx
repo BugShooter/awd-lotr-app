8c99df9bb1d209d55bc03bfeaf04be11eacfe0cb
Alexey Muravyov <muravyov.alexey@gmail.com>
2025-08-13 10:00:02 +0000

    feat: add navigation for previous and next volumes in Volume component

--- src/components/Volume.tsx
+++ src/components/Volume.tsx
@@ -1,7 +1,13 @@
 import { VolumeData } from "@/types/global";
 import Link from "next/link";
+import { volumes } from "../lib/data";
+
 
 export default function Volume({ volumeData }: { volumeData: VolumeData }) {
+    const volumeIndex = volumes.findIndex(v => v.slug === volumeData.slug);
+    const prevVolume = volumeIndex > 0 ? volumes[volumeIndex - 1] : null;
+    const nextVolume = volumeIndex !== -1 && volumeIndex < volumes.length - 1 ? volumes[volumeIndex + 1] : null;
+
     return (
         <>
             <Link href="/volumes">← All Volumes</Link>
@@ -12,6 +18,10 @@ export default function Volume({ volumeData }: { volumeData: VolumeData }) {
                     <li key={book.ordinal}>{book?.ordinal}: {book?.title}</li>
                 ))}
             </ul >
+            <nav className="flex gap-4 items-center flex-row">
+                {prevVolume && (<Link href={prevVolume.slug}>← Prev</Link>)}
+                {nextVolume && (<Link href={nextVolume.slug}>Next →</Link>)}
+            </nav>
         </>
     )
 }
